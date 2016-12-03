76d82e9985633379e50e652b418fcdbd9552b34c
Hitesh <hitesh.ganjoo@buzz4health.com>
2016-12-03 14:33:51 +0530

    test

--- script/convertion_in_file.rb
+++ script/convertion_in_file.rb
@@ -1,8 +1,9 @@
 allText = Dir["#{Dir.pwd}/db/*.txt"]
 allText.each do |textFile|
     puts "Reading file" + textFile
-    textdata = "[" + File.read(textFile) + "]"
-    new_contents = textdata.gsub("}$$${", "},{")
+    textdata = File.read(textFile)
+    new_contents = textdata.gsub( "}{", "}$$${")
     # puts new_contents
+
     File.open(textFile, "w") {|file| file.puts new_contents }
 end
