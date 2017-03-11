35f5254a4ac2c55bb7a5a7ca253792353a57c0ee
sonalbuzz4health <sonal@buzz4health.com>
2017-03-11 13:57:50 +0530

    inclusion of just dial reference model.

---
+++ app/models/reference_justdial.rb
@@ -0,0 +1,29 @@
+class ReferenceJustdial
+  include Mongoid::Document
+  include Mongoid::Timestamps
+   include Mongoid::FullTextSearch
+
+
+  field                     :Emails,      :type => Array,  :default => []
+  field                     :Phones,      :type => Array,  :default => []
+  field                     :ReferenceName,      :type => String, :default => ""
+  field                     :Name,              :type => String, :default => ""
+  field                     :PinCode ,          :type => String, :default => ""
+  field                     :Specialization  , :type => String , :default => ""
+  field                     :Address        ,   :type => String , :default => ""
+  field                     :ClinicName     ,   :type => String  , :default => ""
+  field                     :City           ,   :type => String   , :default => ""
+  field                     :State          ,    :type => String   , :default => ""
+  field                     :ProfileLink    ,    :type => String  ,  :default => ""
+  
+
+ field :unique_key, type: String, :default => ""
+
+
+  validates_uniqueness_of :unique_key
+
+  paginates_per 10000
+  fulltext_search_in :Name, :index_name => 'name_index'
+  fulltext_search_in :ReferenceName,  :index_name => 'ref_name_index'
+
+end
