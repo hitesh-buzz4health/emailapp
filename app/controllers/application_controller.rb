24d5c1fbe9ac25554284b4dca55a0088d7a606e6
sonal <sonal@buzz4health.com>
2017-10-30 19:52:31 +0530

    inclsuion of forgery

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
 
 
+skip_before_action :verify_authenticity_token,
+                     :if => Proc.new { |c| c.request.format == 'application/json' }
+
+
+
+
 def puts_log(msg)
   puts msg
   logger.info msg
