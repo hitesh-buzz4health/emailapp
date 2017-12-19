5f772c8c79def185aad929b894c1b7d3b63fc8fb
Tushar Gupta <your@email.comveeru.ramjas@gmail.com>
2017-12-19 20:05:16 +0530

    Removing dependency of sender accounts from excel sheet

--- app/controllers/gmail_mailer_controller.rb
+++ app/controllers/gmail_mailer_controller.rb
@@ -76,110 +76,128 @@ class GmailMailerController < ApplicationController
     current_page = 0
  
     item_count = users_details.count
-    post_logs "User count: " + item_count.to_s
-    while item_count > 0
-      users_details.skip(current_page * 900).limit(900).each do |user|
-        if !user.nil?
-          begin
-            post_logs "Sending email to user : " + user.name.to_s
-            reciever_detials = get_recievers_details params[:type_database]  , user 
-            reciever_name = reciever_detials["name"]
-            reciever_email = reciever_detials["emails"].to_s                 
-            if reciever_email.length == 0
-              post_logs "Gmes: Receiver email length is 0" 
-              next
-            end    
-            subject = params[:subject_email].clone             
-            if subject.include? "*|FNAME|*"
+    email_accts_required = item_count/1500 + 1
+    @gmail_acct_batch = GmailAccount.where(:status => "unused").limit(email_accts_required)
 
-            subject.gsub! '*|FNAME|*' , reciever_name                          
-
-            end 
-
-            if  current_user.nil?  || total_no_of_mails_for_this_user ==  1500
-              if !current_user.nil?
-                post_logs "Gmes: saving current user data"
-                current_user_row =   get_user email_sheets , current_user
-                email_sheets[current_user_row,5] = "used"
-                email_sheets[current_user_row,6] =  total_no_of_mails_for_this_user
-                email_sheets.save; nil
-                @output_sheet.save; nil
-                total_no_of_mails_for_this_user = 0
-                post_logs "Gmes:Logging out the current user."     
-                gmail = nil 
-                post_logs "Gmes : Putting thread to sleep."
-                sleep 5
-
-              end
-              #getting a new user 
-              user_info = change_user email_sheets 
-              gmail = Gmail.connect( user_info[:email],  user_info[:password])
-              current_user = user_info[:email]
-              user_name  = user_info[:name]
-              #capitalizing the first character of the name .
-              user_name[0] = user_name[0].capitalize
-              
-              post_logs "Gmes : current user for this instance." + current_user.to_s
-            end 
-            template = params[:html_body].clone
-            template.gsub! '*|FNAME|*' , reciever_name.to_s 
-            template.gsub! '*|Email|*' ,  reciever_email.to_s
-            template.gsub! '*|Title|*' ,   params[:main_title] 
-            template.gsub! '*|Description|*' ,  params[:main_description]  
-            template.gsub! '*|ActionUrl|*' ,   params[:Action_url]   
-            template.gsub! '*|ImageUrl|*' , params[:Image_url]   
-            # removing new line if any exists.
-            template.delete!("\n")
-            post_logs "Gmes : mail is being sent to " + reciever_name + " " +reciever_email 
-            email = gmail.compose do
-              to reciever_email
-              from  user_name
-              subject  subject.to_s
-                #for adding html template 
-              html_part do
-                content_type 'text/html; charset=UTF-8'
-                body  template.to_s
-              end                              
-            end
-            #delivering email
-            email.deliver!
-            post_logs "Gmes : Email send user put to sleep" 
-            sleep 5
-
-            total_no_of_mails_for_this_user = total_no_of_mails_for_this_user  + 1
-            total_no_of_mails_for_the_day = total_no_of_mails_for_the_day + 1 
-
-            # post_output user_name , reciever_name , reciever_email , subject , total_no_of_mails_for_the_day
-
-            post_logs "Gmes : no of mails sent for this user is " +total_no_of_mails_for_this_user.to_s
-            post_logs "Gmes : no of total  mails for this session " +total_no_of_mails_for_the_day.to_s    
-            post_logs "Gmes : total no of mails for this session "  +users_details.count.to_s
-            post_logs "Gmes : total no of errors for this session "  +total_no_of_errors.to_s
-
-          rescue  Exception => e        
+    if @gmail_acct_batch.count >= email_accts_required
+      @gmail_acct_batch.update_all(status: "inuse")
+      current_sender_acct_index = 0
+      post_logs "User count: " + item_count.to_s
+      while item_count > 0
+        users_details.skip(current_page * 900).limit(900).each do |user|
+          if !user.nil?
             begin
-              total_no_of_errors = total_no_of_errors + 1 
-              @output_sheet[@num_of_rows , 6] = e
-              @output_sheet.save; nil        
-            rescue  Exception => e
+              post_logs "Sending email to user : " + user.name.to_s
+              reciever_detials = get_recievers_details params[:type_database]  , user 
+              reciever_name = reciever_detials["name"]
+              reciever_email = reciever_detials["emails"].to_s                 
+              if reciever_email.length == 0
+                post_logs "Gmes: Receiver email length is 0" 
+                next
+              end    
+              subject = params[:subject_email].clone             
+              if subject.include? "*|FNAME|*"
+
+              subject.gsub! '*|FNAME|*' , reciever_name                          
+
+              end 
+
+              if  current_user.nil?  || total_no_of_mails_for_this_user ==  1500
+                if !current_user.nil?
+                  post_logs "Gmes: saving current user data"
+                  #current_user_row =   get_user email_sheets , current_user
+                  #email_sheets[current_user_row,5] = "used"
+                  #email_sheets[current_user_row,6] =  total_no_of_mails_for_this_user
+                  #email_sheets.save; nil
+                  gmail_acct_batch[current_sender_acct_index].total_emails_sent = 1500
+                  gmail_acct_batch[current_sender_acct_index].status = 'used'
+                  gmail_acct_batch[current_sender_acct_index].save!
+                  current_sender_acct_index = current_sender_acct_index + 1
+                  @output_sheet.save; nil
+                  total_no_of_mails_for_this_user = 0
+                  post_logs "Gmes:Logging out the current user."     
+                  gmail = nil 
+                  post_logs "Gmes : Putting thread to sleep."
+                  sleep 5
+
+                end
+                #getting a new user 
+                #user_info = change_user email_sheets
+                
+                user_info = get_sender_user_info current_sender_acct_index
+                gmail = Gmail.connect( user_info[:email],  user_info[:password])
+                current_user = user_info[:email]
+                user_name  = user_info[:name]
+                #capitalizing the first character of the name .
+                user_name[0] = user_name[0].capitalize
+                
+                post_logs "Gmes : current user for this instance." + current_user.to_s
+              end 
+              template = params[:html_body].clone
+              template.gsub! '*|FNAME|*' , reciever_name.to_s 
+              template.gsub! '*|Email|*' ,  reciever_email.to_s
+              template.gsub! '*|Title|*' ,   params[:main_title] 
+              template.gsub! '*|Description|*' ,  params[:main_description]  
+              template.gsub! '*|ActionUrl|*' ,   params[:Action_url]   
+              template.gsub! '*|ImageUrl|*' , params[:Image_url]   
+              # removing new line if any exists.
+              template.delete!("\n")
+              post_logs "Gmes : mail is being sent to " + reciever_name + " " +reciever_email 
+              email = gmail.compose do
+                to reciever_email
+                from  user_name
+                subject  subject.to_s
+                  #for adding html template 
+                html_part do
+                  content_type 'text/html; charset=UTF-8'
+                  body  template.to_s
+                end                              
+              end
+              #delivering email
+              email.deliver!
+              post_logs "Gmes : Email send user put to sleep" 
+              sleep 5
+
+              total_no_of_mails_for_this_user = total_no_of_mails_for_this_user  + 1
+              total_no_of_mails_for_the_day = total_no_of_mails_for_the_day + 1 
+
+              # post_output user_name , reciever_name , reciever_email , subject , total_no_of_mails_for_the_day
+
+              post_logs "Gmes : no of mails sent for this user is " +total_no_of_mails_for_this_user.to_s
+              post_logs "Gmes : no of total  mails for this session " +total_no_of_mails_for_the_day.to_s    
+              post_logs "Gmes : total no of mails for this session "  +users_details.count.to_s
+              post_logs "Gmes : total no of errors for this session "  +total_no_of_errors.to_s
+
+            rescue  Exception => e        
+              begin
+                total_no_of_errors = total_no_of_errors + 1 
+                @output_sheet[@num_of_rows , 6] = e
+                @output_sheet.save; nil        
+              rescue  Exception => e
+                post_logs  "GMES: caught exception #{e}! ohnoes!"
+              end
+
               post_logs  "GMES: caught exception #{e}! ohnoes!"
             end
-
-            post_logs  "GMES: caught exception #{e}! ohnoes!"
           end
         end
+        item_count-=900
+        current_page+=1
       end
-      item_count-=900
-      current_page+=1
+    else
+      post_logs "No Sufficient email accounts avalable for this campaign"
     end
      #saving sheet in case when emails are less
     post_logs "Gmes : finishing up the script" 
     send_results(total_no_of_mails_for_the_day , users_details.count.to_s , start_time , params[:subject_email] ,  total_no_of_errors.to_s)
     post_logs "Gmes : result has been sent."
     @output_sheet.save; nil
-    email_sheets[current_user_row,5] = "used"
-    email_sheets[current_user_row,6] =  total_no_of_mails_for_this_user
-    email_sheets.save; nil
+    #email_sheets[current_user_row,5] = "used"
+    #email_sheets[current_user_row,6] =  total_no_of_mails_for_this_user
+    #email_sheets.save; nil
+    gmail_acct_batch[current_sender_acct_index].total_emails_sent = 1500
+    gmail_acct_batch[current_sender_acct_index].status = 'used'
+    gmail_acct_batch[current_sender_acct_index].save!
     gmail.logout
   end 
 
@@ -476,6 +494,14 @@ class GmailMailerController < ApplicationController
 	   end 
 
 	end
+
+  def get_sender_user_info index 
+    user_info = Hash.new
+    user_info[:name] = @gmail_acct_batch[index].first_name + " From Buzz4health" 
+    user_info[:email] = @gmail_acct_batch[index].email
+    user_info[:password] = @gmail_acct_batch[index].pass
+    return user_info
+  end
 
 	def get_user email_sheets , current_user
 
---
+++ app/models/gmail_account.rb
@@ -0,0 +1,12 @@
+class GmailAccount
+  include Mongoid::Document
+  include Mongoid::Timestamps 
+
+  field :email,                      :type => String,  :default => ''
+  field :pass,                       :type => String
+  field :status,                     :type => String, :default => 'unused'
+  field :activity_at,                :type => Time
+  field :first_name,                 :type => String, :default => 'Sheerin'
+  field :total_emails_sent,          :type => Integer, :default => 0
+
+end
