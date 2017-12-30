0e2290327cf6e7418c82d0da198b72d884333acf
Tushar Gupta <your@email.comveeru.ramjas@gmail.com>
2017-12-30 15:12:25 +0530

    Fixed email name using related issue

--- app/models/gmail_account.rb
+++ app/models/gmail_account.rb
@@ -4,7 +4,7 @@ class GmailAccount
 
   field :email,                      :type => String,  :default => ''
   field :pass,                       :type => String
-  field :status,                     :type => String, :default => 'unused'
+  field :status,                     :type => String, :default => 'unused' # {inuse, used} 
   field :activity_at,                :type => Time
   field :first_name,                 :type => String, :default => 'Sheerin'
   field :total_emails_sent,          :type => Integer, :default => 0
